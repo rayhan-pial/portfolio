0c5034d8c6a5b2d39a617f79de908d5b69fd02f5
Rayhan <pialshek08@gmail.com>
2024-01-11 10:15:36 +0600

    vacel setup

---
+++ build_files.sh
@@ -0,0 +1,9 @@
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
+
+echo "Make Migrations"
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static"
+python3.9 manage.py collectstatic --noinput --clear
